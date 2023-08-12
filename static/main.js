ffcd8493beb143e5fb45b524bd6bcb47ce7b712f
Roberto Gomes <91014834+jr-roberto@users.noreply.github.com>
2023-08-11 21:20:06 -0300

    Ajustes

--- static/main.js
+++ static/main.js
@@ -1,3 +1,10 @@
 document.body.style.height = `${window.innerHeight}px`
 
 window.addEventListener("resize",()=>{document.body.style.height = `${window.innerHeight}px`} )
+
+function selecionarVeiculo(elment){
+    let placa = elment.getAttribute("placa");
+    let status = elment.getAttribute("status-veiculo");
+
+    console.log(`O veiculo ${placa} esta ${status}`);
+}
